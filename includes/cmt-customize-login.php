84a0c8775abd23511ad17e69cbb11585285bb96f
Ubuntu <ubuntu@ip-172-31-21-92.us-west-2.compute.internal>
2018-04-16 02:26:27 +0000

    Added CSS for custom logo.

--- includes/cmt-customize-login.php
+++ includes/cmt-customize-login.php
@@ -10,8 +10,24 @@ if (!class_exists(CMT_Customize_Login_Screen)) :
    */
   class CMT_Customize_Login_Screen {
     public static function init() {
-      
+      add_action("login_enqueue_scripts", array($this, "changeDefaultLogo"));
     } // init()
+    
+    /**
+     * Changes the logo from WordPress to CMT.
+     */
+    private function changeDefaultLogo() { ?>
+      <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url("https://dv1ko3cnumxhb.cloudfront.net/celmatrixtechnologies.com/images/logo-wp-login.jpg");
+            height:65px;
+            width:320px;
+            background-size: 320px 65px;
+            background-repeat: no-repeat;
+                  padding-bottom: 30px;
+          }
+      </style>
+    <?php } // changeDefaultLogo()
   } // CMT_Customize_Login_Screen
 
 endif; // class exists check
